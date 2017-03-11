42bd7df433436873d71ee182f2a3e0c4b6e87fb3
Philipp Renoth <renoth@wildoak.de>
2017-03-11 23:51:17 +0100

    initial commit

---
+++ proton.js
@@ -0,0 +1,302 @@
+import React from 'react';
+
+
+
+
+
+
+
+
+
+export class Proton {
+    subscriptionId = 1;
+    subscriptions = {};
+
+    size;
+
+    protonName = 'unknown';
+    protonFactor = 0;
+
+    breakpoints;
+    protonNameMin;
+    breakpointNameToIndex;
+    minBreakpoint;
+    maxBreakpoint;
+
+    constructor(
+        protonNameMin = 'xxs',
+        breakpoints = {xs: 280, s: 340, m: 576, l: 768, xl: 992, xxl: 1200}
+    ) {
+        this.protonNameMin = protonNameMin;
+
+        const keys = Object.keys(breakpoints);
+        if (keys.length < 2) {
+            throw new Error(`At least 2 breakpoints must be given. ${keys.length} given`);
+        }
+
+        this.minBreakpoint = keys.reduce(
+            (acc, key) => acc !== undefined ? Math.min(acc, breakpoints[key]) : breakpoints[key],
+            undefined
+        );
+        this.maxBreakpoint = keys.reduce(
+            (acc, key) => acc !== undefined ? Math.max(acc, breakpoints[key]) : breakpoints[key],
+            undefined
+        );
+
+
+
+        this.breakpoints = keys.map(key => {
+            const name = key;
+            const breakpoint = breakpoints[key];
+
+            return {name, breakpoint};
+        });
+
+        this.breakpoints.sort((a, b) => {
+            if (a.breakpoint < b.breakpoint) {
+                return -1;
+            }
+
+            if (a.breakpoint > b.breakpoint) {
+                return 1;
+            }
+
+            throw new Error(`breakpoints should not have identical values: ${a.name}:${a.breakpoint} vs. ${b.name}:${b.breakpoint}`);
+        });
+
+        this.breakpointNameToIndex = {
+            [protonNameMin]: -1
+        };
+        this.breakpoints.forEach((breakpoint, index) => {
+            this.breakpointNameToIndex[breakpoint.name] = index;
+        });
+    }
+
+    subscribe(cb) {
+        const id = this.subscriptionId++;
+        this.subscriptions[id] = cb;
+        this.callBack(cb);
+
+        return () => delete this.subscriptions[id];
+    }
+
+    callBack(cb) {
+        cb(this.protonFactor);
+    }
+
+    setSize(size) {
+        this.size = size;
+        this.update();
+
+        const keys = Object.keys(this.subscriptions);
+        keys.forEach(key => this.callBack(this.subscriptions[key]));
+    }
+
+    factor(w, min, max) {
+        return Math.min(1, Math.max(0, (w - min) / (max - min)));
+    };
+
+    update() {
+        this.protonName = this.protonNameMin;
+        for (let i = 0; i < this.breakpoints.length; i++) {
+            const breakpoint = this.breakpoints[i];
+
+            if (this.size < breakpoint.breakpoint) {
+                break;
+            }
+
+            this.protonName = breakpoint.name;
+        }
+
+        this.protonFactor = this.factor(this.size, this.minBreakpoint, this.maxBreakpoint);
+    }
+
+    clear() {
+        this.subscriptions = {};
+    }
+}
+
+export class ProtonProvider extends React.Component {
+
+    proton = new Proton();
+
+    handleResize = evt => {
+        this.proton.setSize(evt.target.innerWidth);
+    };
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+        this.proton.setSize(window.innerWidth);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+        this.proton.clear();
+    }
+
+    getChildContext() {
+        return {
+            proton: this.proton
+        };
+    }
+
+    render() {
+        return this.props.children;
+    }
+
+}
+
+ProtonProvider.childContextTypes = {
+    proton: React.PropTypes.any.isRequired
+};
+
+
+
+
+
+
+
+
+
+
+export const protonize = (Component) => {
+
+    class Protonized extends React.Component {
+        
+        unsubscribe;
+
+        constructor(props, context) {
+            super(props, context);
+
+            this.state = {
+                protonFactor: 1
+            };
+        }
+
+        componentDidMount() {
+            this.unsubscribe = this.context.proton.subscribe(
+                protonFactor => this.setState({protonFactor})
+            );
+        }
+
+        componentWillUnmount() {
+            this.unsubscribe();
+        }
+
+        render() {
+
+            return (
+                <Component
+                    {...this.props}
+                    proton={this.context.proton}
+                    protonFactor={this.state.protonFactor}
+                />
+            );
+        }
+    }
+
+    Protonized.contextTypes = {
+        proton: React.PropTypes.any.isRequired
+    };
+
+
+    return Protonized;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Electron_ = (props, context) => {
+    const style = {
+        ...protonStyle(props.style, context.proton),
+        display: 'inline-block'
+    };
+
+    if (props.cols) {
+        let protonName = props.proton.protonName;
+        let protonIndex = context.proton.breakpointNameToIndex[protonName];
+        
+        if (protonIndex !== undefined) {
+            while(!(protonName in props.cols)) {
+                if (protonIndex === -1) {
+                    break;
+                }
+
+                protonName = context.proton.breakpoints[protonIndex].name;
+                protonIndex--;
+            }
+
+            const factor = protonIndex !== -1 ? props.cols[protonName] : 1;
+            style.width = `${factor*100}%`;
+        }
+        
+    }
+
+    return (
+        <div style={style}>
+            {props.children}
+        </div>
+    );
+};
+
+Electron_.contextTypes = {
+    proton: React.PropTypes.any.isRequired
+};
+
+export const Electron = protonize(Electron_);
+
+
+export const protonStyle = (protonStyle_, proton) => {
+    if (!protonStyle_) {
+        return null;
+    }
+
+    const style = {};
+    const keys = Object.keys(protonStyle_);
+    keys.forEach(key => {
+        const attr = protonStyle_[key];
+        
+        if (Array.isArray(attr)) {
+            style[key] = attr[0] + attr[1] * proton.protonFactor;
+        } else if (typeof attr === 'object') {
+            let protonName = proton.protonName;
+            let protonIndex = proton.breakpointNameToIndex[protonName];
+            
+            if (protonIndex !== undefined) {
+                while(!(protonName in attr)) {
+                    
+                    if (protonIndex === -1) {
+                        protonName = proton.protonNameMin;
+                        break;
+                    }
+                    
+                    protonName = proton.breakpoints[protonIndex].name;
+                    protonIndex--;
+                }
+
+                if (protonName in attr) {
+                    style[key] = attr[protonName];
+                }
+            }
+        } else {
+            style[key] = attr;
+        }
+    });
+
+    return style;
+};
---
+++ proton.test.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { Electron, Proton, protonStyle } from './proton';
+import 'jasmine';
+
+const asyncTest = run => done => run().then(done, err => { fail(err); done(); });
+
+describe('Proton', () => {
+    it('should calculate factors and derive proton names', () => {
+        const s1 = 100;
+        const s2 = 200;
+        
+        const proton = new Proton('s0', {s1, s2});
+
+        proton.setSize(50);
+        expect(proton.protonName).toBe('s0');
+        expect(proton.protonFactor).toBe(0);
+
+        proton.setSize(99);
+        expect(proton.protonName).toBe('s0');
+        expect(proton.protonFactor).toBe(0);
+
+        proton.setSize(100);
+        expect(proton.protonName).toBe('s1');
+        expect(proton.protonFactor).toBe(0);
+
+        proton.setSize(101);
+        expect(proton.protonName).toBe('s1');
+        expect(proton.protonFactor).toBe(1/100);
+
+        proton.setSize(199);
+        expect(proton.protonName).toBe('s1');
+
+        proton.setSize(200);
+        expect(proton.protonName).toBe('s2');
+
+        proton.setSize(201);
+        expect(proton.protonName).toBe('s2');
+
+        proton.setSize(2349802384);
+        expect(proton.protonName).toBe('s2');
+    });
+
+    it('should subscribe and unsubscribe', () => {
+        const s1 = 100;
+        const s2 = 200;
+        const proton = new Proton('s0', {s1, s2});
+
+        const spy1 = jasmine.createSpy();
+        const spy2 = jasmine.createSpy();
+
+        proton.setSize(100);
+        const unsubscribe1 = proton.subscribe(spy1);
+
+        expect(spy1).toHaveBeenCalledWith(0);
+        expect(spy2).not.toHaveBeenCalled();
+
+        const unsubscribe2 = proton.subscribe(spy2);
+
+        proton.setSize(150);
+        expect(spy1).toHaveBeenCalledWith(0.5);
+        expect(spy2).toHaveBeenCalledWith(0.5);
+
+        unsubscribe1();
+
+        proton.setSize(200);
+        expect(spy1).not.toHaveBeenCalledWith(1);
+        expect(spy2).toHaveBeenCalledWith(1);
+    });
+
+    it('should calculate breakpoint name to indices', () => {
+        const s1 = 100;
+        const s2 = 200;
+        const s3 = 300;
+        const proton = new Proton('s0', {s2, s3, s1}); // shuffled breakpoints
+
+        expect(proton.breakpointNameToIndex).toEqual({
+            s0: -1,
+            s1: 0,
+            s2: 1,
+            s3: 2
+        });
+    });
+});
+
+describe('<Electron />', () => {
+
+    it('should render without crashing', () => {
+        const wrapper = mount(<Electron />, {
+            context: {proton: new Proton()}
+        });
+    });
+
+    it('should generate correct html', () => {
+        const s1 = 100;
+        const s2 = 200;
+
+        const proton = new Proton('s0', {s1, s2});
+        proton.setSize(100);
+
+        const wrapper = mount(<Electron cols={{s0: 1, s1: 0.5, s2: 0.25}}>child</Electron>, {
+            context: {proton}
+        });
+        
+        expect(wrapper.html()).toBe('<div style="display: inline-block; width: 50%;">child</div>');
+
+        proton.setSize(500);
+        expect(wrapper.html()).toBe('<div style="display: inline-block; width: 25%;">child</div>');
+    });
+
+    it('should use 100% if no col definition found', () => {
+        const s1 = 100;
+        const s2 = 200;
+
+        const proton = new Proton('s0', {s1, s2});
+        proton.setSize(100);
+
+        const wrapper = mount(<Electron cols={{s2: 0.25}}>child</Electron>, {
+            context: {proton}
+        });
+        
+        expect(wrapper.html()).toBe('<div style="display: inline-block; width: 100%;">child</div>');
+    });
+
+});
+
+describe('protonStyle', () => {
+    let proton;
+
+    beforeEach(() => {
+        const s1 = 100;
+        const s2 = 200;
+
+        proton = new Proton('s0', {s1, s2});
+        proton.setSize(0);
+    });
+
+    it('should return empty object empty style object', () => {
+        expect(protonStyle({}, proton)).toEqual({});
+    });
+
+    it('should return null for falsy style', () => {
+        expect(protonStyle(null, proton)).toBe(null);
+        expect(protonStyle(undefined, proton)).toBe(null);
+    });
+
+    it('should calculate style according to proton factor and proton name', () => {
+        const style = {
+            style1: 'value1',
+            style2: [10, 50],
+            style3: {
+                s0: 'value1',
+                s1: 'value2',
+                s2: 'value3'
+            }
+        };
+        expect(protonStyle(style, proton)).toEqual({
+            style1: 'value1',
+            style2: 10,
+            style3: 'value1'
+        });
+    });
+    
+    it('should omit style if not match', () => {
+        const style = {
+            style1: 'value1',
+            style2: [10, 50],
+            style3: {
+                s2: 'value3'
+            }
+        };
+        expect(protonStyle(style, proton)).toEqual({
+            style1: 'value1',
+            style2: 10
+        });
+    });
+});
